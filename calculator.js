98c4acbb8967e60abefc4977ab9f1bda77f4cfc4
filetopenguin <arielweiser17@gmail.com>
2023-11-23 17:32:43 +0200

    finishing up javascript

--- calculator.js
+++ calculator.js
@@ -1,20 +1,65 @@
-let op1 =0;
-let op2 =0;
+let op1 ="";
+let op2 ="";
 let operator = "";
 
 
 const btnNum = document.querySelectorAll('.num');
 const btnOp = document.querySelectorAll('.op');
+const screen = document.querySelector('.screen');
+const clear = document.querySelector('.clear');
+const solve = document.querySelector('.solve');
+
+solve.addEventListener('click', ()=>{
+    switch(operator){
+        case "+":
+           screen.textContent = add(op1,op2);
+           clearAll();
+            break;
+        case "-":
+            screen.textContent = subtract(op1,op2);
+            clearAll();
+            break;
+        case "/":
+            screen.textContent = divide(op1,op2);
+            clearAll();
+            break;
+        case "*":
+            screen.textContent = multiply(op1,op2);
+            clearAll();
+            break;
+        default:
+            clearAll();
+    }
+    
+})
+
+function clearAll(){
+    operator="";
+    op1 ="";
+    op2="";
+}
+clear.addEventListener('click',()=>{
+   clearAll();
+   screen.textContent="";
+});
 
 btnOp.forEach((op)=>{
     op.addEventListener('click', ()=>{
-        console.log(op.textContent);
+        operator = op.textContent;
+   //     screen.textContent += " "+ operator + " ";
     });
     
 });
+
 btnNum.forEach((num)=>{
     num.addEventListener('click',() =>{
-     console.log(num.textContent);
+        if(operator!=""){
+            op2 += num.textContent;
+            screen.textContent = op1 + " "+ operator + " " + op2;
+        }else{
+        op1 += num.textContent;
+        screen.textContent = op1;
+        }
     });
    
 });
@@ -22,7 +67,7 @@ btnNum.forEach((num)=>{
 
 //calculator functions
 function add(num1,num2){
-    return num1+num2;
+    return parseInt(num1)+parseInt(num2);
 }
 function subtract(num1,num2){
     return num1-num2;
